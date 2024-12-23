8a2b79e840841138c751ea84f5a308ce1e7ae272
Lucau Rafael <72351063+luradev@users.noreply.github.com>
2024-12-23 12:54:32 +0100

    First commit

---
+++ admin-cliente/src/main/java/com/admincliente/admin_cliente/AdminClienteApplication.java
@@ -0,0 +1,13 @@
+package com.admincliente.admin_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminClienteApplication.class, args);
+	}
+
+}
